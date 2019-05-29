2d8cffeb2cd99e359415df8bc44a7d1b4282a1d2
Martin McDaid <martin.mcdaid.etb@gmail.com>
2019-05-29 11:15:47 +0100

    Add project files.

---
+++ PieShopDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PieShopDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
